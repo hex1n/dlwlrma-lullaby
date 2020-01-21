c15cc09cba45fa1a5094553954553d27ef06c168
hex1n <mm602005>
2020-01-21 10:52:35 +0800

    构建Multi zone eureka server

---
+++ ch3-2/ch3-2-eureka-client/src/main/java/com/dlwlrma/lullaby/controller/QueryController.java
@@ -0,0 +1,24 @@
+package com.dlwlrma.lullaby.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author:hex1n
+ * @Date:2020/1/20 17:40
+ */
+@RestController
+@RequestMapping("/query")
+public class QueryController {
+
+    @Autowired
+    EurekaClientConfigBean eurekaClientConfigBean;
+
+    @GetMapping("/eureka-server")
+    public Object getEurekaServerUlr() {
+        return eurekaClientConfigBean.getServiceUrl();
+    }
+}
---
+++ ch3-2/ch3-2-zuul-gateway/src/main/java/com/dlwlrma/lullaby/Ch32ZuulGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.dlwlrma.lullaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author hex1n
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class Ch32ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch32ZuulGatewayApplication.class, args);
+    }
+
+}
